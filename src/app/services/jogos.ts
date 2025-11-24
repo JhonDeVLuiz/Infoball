aea8cd7c79dce8aae058a60366f04f89d12340a4
JhonDevLuiz <devjhon01010@gmail.com>
2025-11-23 23:31:07 -0300

    Revert "v4"
    This reverts commit 8a61928be364dda936e275949b76caa8a21c0759.

--- src/app/services/jogos.ts
+++ src/app/services/jogos.ts
@@ -12,7 +12,7 @@ export class JogosService {
   public error = '';
 
   // Se testar no celular substitua "localhost" pelo IP da sua máquina: ex 'http://192.168.0.100:3000/jogos'
-  private apiUrl = ' https://backend-infoboll.onrender.com';
+  private apiUrl = 'http://192.168.0.108:3000';
 
   constructor(private http: HttpClient) {}
 
